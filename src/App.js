2f8bef4abd081e347454897b0aab6764c39e4de4
Dániel Kántor <git@daniel-kantor.com>
2018-03-14 15:05:43 +0100

    feat: Add basic framework

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Grid, Row, Col, FormControl } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Grid>
+        <Row>
+          <h1>parldata-frontend</h1>
+        </Row>
+        <Row>
+          <Col sm={3}>
+            <h2>Search</h2>
+            <FormControl />
+          </Col>
+          <Col sm={9}>
+            <h2>Keresés eredménye</h2>
+          </Col>
+        </Row>
+      </Grid>
+    )
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<App />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+})
