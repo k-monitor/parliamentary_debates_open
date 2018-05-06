c5d827376283a3199f7fd6fd36291232f8595b6b
Dániel Kántor <git@daniel-kantor.com>
2018-05-06 09:07:39 +0200

    Separate multiple topics by a comma

--- src/Results.js
+++ src/Results.js
@@ -11,7 +11,11 @@ const Results = ({ results, page, navigate_to_page }) => (
   {results.hits.hits.map(result => (
     <article className="result" key={ result._id }>
       <h1>
-        <a href="#"><b>{result._source.topic}</b> &mdash; {result._source.speaker}</a>
+        <a href="#"><b>{
+            Array.isArray(result._source.topic)
+              ? result._source.topic.join(', ')
+              : result._source.topic
+        }</b> &mdash; {result._source.speaker}</a>
         <span className="meta">{result._source.date},  ({result._source.session}, {result._source.sitting_type})</span>
       </h1>
       <p dangerouslySetInnerHTML={{__html: result.highlight.text}} />
