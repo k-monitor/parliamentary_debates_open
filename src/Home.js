dc2c82a2392287cf5cf3dddc5bac313f809fe17a
Daniel Kantor <git@daniel-kantor.com>
2019-02-12 19:48:25 +0100

    add keyword cards to home page

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {Row, Col, FormControl} from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  FormControl,
+  Card,
+  Button,
+  CardColumns,
+} from 'react-bootstrap';
 import {navigate_to_search} from './store/modules/search';
 import {open_modal} from './store/modules/search';
 import {close_modal} from './store/modules/search';
@@ -12,6 +19,7 @@ import Help from './Help.js';
 import {DelayInput} from 'react-delay-input';
 import Loading from 'react-loading-bar';
 import Datetime from 'react-datetime';
+import HomepageKeywords from './homepage_keywords.json';
 require('moment/locale/hu');
 
 const parseDate = string =>
@@ -23,13 +31,14 @@ const Home = props => (
   <>
     <Row>
       <Loading show={props.search.loading} color="red" showSpinner={false} />
-      <Col sm={3}>
+      <Col sm={props.search.term === '' ? 12 : 3}>
         <h2>Kereső</h2>
         <DelayInput
           minLength={3}
           delayTimeout={300}
           element={FormControl}
           value={props.search.term}
+          placeholder="Kezdjen el gépelni egy keresőkifejezést"
           onChange={event =>
             props.navigate_to_search({
               ...props.search,
@@ -37,6 +46,25 @@ const Home = props => (
             })
           }
         />
+        <CardColumns style={{marginTop: '3em'}}>
+          {props.search.term === '' &&
+            HomepageKeywords.map(keyword => (
+              <Card key={keyword}>
+                <Card.Body>
+                  <Card.Title>{keyword}</Card.Title>
+                  <Button
+                    variant="primary"
+                    onClick={() =>
+                      props.navigate_to_search({
+                        term: keyword,
+                      })
+                    }>
+                    Keresés
+                  </Button>
+                </Card.Body>
+              </Card>
+            ))}
+        </CardColumns>
         {props.search.term && props.search.term.length >= 3 ? (
           <div className="filterTools">
             <h2>Felszólalók</h2>
@@ -81,7 +109,7 @@ const Home = props => (
           </div>
         ) : null}
       </Col>
-      <Col sm={9}>
+      <Col sm={props.search.term === '' ? 0 : 9}>
         <Help toggle={props.toggle} show={props.help.show} />
         <Results
           results={props.search.results}
