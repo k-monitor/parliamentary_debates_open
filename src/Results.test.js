3e0c289675f43270fe73f6650955d415a429849d
Dániel Kántor <git@daniel-kantor.com>
2019-04-29 21:14:20 +0200

    make CI green again

--- src/Results.test.js
+++ src/Results.test.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import Results from './Results';
-import renderer from 'react-test-renderer';
+import React from "react";
+import Results from "./Results";
+import renderer from "react-test-renderer";
 
-it('renders correctly', () => {
+it("renders correctly", () => {
   const tree = renderer
     .create(
       <Results
         results={{
           hits: {
+            total: { value: 0 },
             hits: [
-              {highlight: {text: 'a'}, _source: {text: 'Foo'}, _id: 3},
-              {highlight: {text: 'b'}, _source: {text: 'Bar'}, _id: 2},
-            ],
-          },
+              { highlight: { text: "a" }, _source: { text: "Foo" }, _id: 3 },
+              { highlight: { text: "b" }, _source: { text: "Bar" }, _id: 2 }
+            ]
+          }
         }}
-      />,
+      />
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
