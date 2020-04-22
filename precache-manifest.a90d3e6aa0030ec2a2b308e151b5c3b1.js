c34ce937c08ec185b0f473ea26f91e169cdd2969
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-22 09:07:39 +0000

    Deploy k-monitor/parliamentary_debates_open to github.com/k-monitor/parliamentary_debates_open.git:gh-pages

---
+++ precache-manifest.a90d3e6aa0030ec2a2b308e151b5c3b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63dbc29c6bd3384d7ee0b8e7f9f94523",
+    "url": "/parliamentary_debates_open/index.html"
+  },
+  {
+    "revision": "1d5bc094313c97d52a48",
+    "url": "/parliamentary_debates_open/static/css/2.8a06d3cc.chunk.css"
+  },
+  {
+    "revision": "986e19ede321024d6eef",
+    "url": "/parliamentary_debates_open/static/css/main.16f50902.chunk.css"
+  },
+  {
+    "revision": "1d5bc094313c97d52a48",
+    "url": "/parliamentary_debates_open/static/js/2.9cb40007.chunk.js"
+  },
+  {
+    "revision": "986e19ede321024d6eef",
+    "url": "/parliamentary_debates_open/static/js/main.82736c11.chunk.js"
+  },
+  {
+    "revision": "e330df916da7db3fa421",
+    "url": "/parliamentary_debates_open/static/js/runtime-main.e5011e2d.js"
+  },
+  {
+    "revision": "c449bb8f19d7e20d6956edce2bf06089",
+    "url": "/parliamentary_debates_open/static/media/logo.c449bb8f.jpg"
+  }
+]);
