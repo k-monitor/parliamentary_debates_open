5ffe52fec2bda147c47048a82b93dfcae09a4470
Miklos Gabor <gmiki91@gmail.com>
2024-07-05 18:15:16 +0200

    wider clickable monthly chart

--- src/Chart.js
+++ src/Chart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
   result_count_by_date,
   bin,
@@ -6,13 +7,39 @@ import {
   formatBinNameShort,
   formatBinNameMini,
 } from './transformations';
-import {AreaChart, Area, XAxis, YAxis, Tooltip} from 'recharts';
+import { XAxis, YAxis, Tooltip, BarChart, Bar } from 'recharts';
+import { navigate_to_search } from './store/modules/search';
 
-const binsize = 5184000000 * 3;
+const dayInMs = 1000 * 60 * 60 * 24;
+const binsize = dayInMs * 30
 
-const Chart = props => (
+const Chart = props => {
+
+  const onMonthClick = data => {
+    const binStart = new Date(Math.floor((data.timestamp * 1) / binsize) * binsize);
+    const binEnd = new Date(Math.floor((data.timestamp * 1) / binsize) * binsize + binsize);
+    const startDate = `${binStart.getFullYear()}.${String(binStart.getMonth() + 1).padStart(2, 0)}.${binStart.getDate()}`;
+    const endDate = `${binEnd.getFullYear()}.${String(binEnd.getMonth() + 1).padStart(2, 0)}.${binEnd.getDate()}`;
+
+    props.navigate_to_search({
+      ...props.search,
+      start_date: startDate,
+      end_date: endDate
+    })
+  }
+
+  const onYearClick = timestamp => {
+    const year = new Date(timestamp).getFullYear();
+    props.navigate_to_search({
+      ...props.search,
+      start_date: `${year}.01.01`,
+      end_date: `${year}.12.31`,
+    })
+  }
+
+  return (
   <div style={props.style}>
-    <AreaChart
+    <BarChart
       width={props.width}
       height={props.height}
       data={bin(binsize)(result_count_by_date(props))}
@@ -21,7 +48,7 @@ const Chart = props => (
           ? {top: 5, right: 10, bottom: 20, left: 5}
           : {top: 5, right: 20, bottom: 80, left: 5}
       }>
-      <Area
+      <Bar
         type="monotone"
         dataKey="count"
         name="Felszólalások száma"
@@ -33,20 +60,20 @@ const Chart = props => (
         strokeOpacity={1}
         strokeWidth={2}
         isAnimationActive={false}
+        onClick={onMonthClick}
       />
       <XAxis
         tick={{fill: '#ebebeb'}}
         type="number"
         dataKey="timestamp"
-        angle={props.mini ? 0 : -45}
+        angle={props.mini ? 0 : -60}
         textAnchor="end"
         domain={['dataMin', 'dataMax']}
-        minTickGap={props.mini ? 10 : -35}
+        minTickGap={props.mini ? 10 : -15}
         tickCount={100}
         ticks={bin(binsize)(result_count_by_date(props)).map(i => i.timestamp)}
-        tickFormatter={
-          props.mini ? formatBinNameMini(binsize) : formatBinNameShort(binsize)
-        }
+        tickFormatter={formatBinNameMini(binsize)}
+        onClick={data => onYearClick(data.value)}
       />
       {!props.mini && <YAxis tick={{fill: '#ebebeb'}} />}
       {!props.mini && (
@@ -58,8 +85,17 @@ const Chart = props => (
           itemStyle={{color: 'black'}}
         />
       )}
-    </AreaChart>
+    </BarChart>
   </div>
 );
+}
 
-export default Chart;
+const mapStateToProps = state => ({
+  search: state.search,
+});
+
+const mapDispatchToProps = {
+  navigate_to_search,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chart);
