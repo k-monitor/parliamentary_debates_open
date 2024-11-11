e5e8284a120439142ef2f2bc2882d44f8de75a7a
boa <boa@protonmail.com>
2024-11-11 22:52:08 +0100

    Updates

---
+++ precache-manifest.961a6df55c249561ae48e490addd65c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ce2a55c530c6fe8c699858f23b247f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa38781b7ab90ffac47f",
+    "url": "/static/css/2.5c7438d8.chunk.css"
+  },
+  {
+    "revision": "66b91629dffba9d08872",
+    "url": "/static/css/main.63245cf7.chunk.css"
+  },
+  {
+    "revision": "fa38781b7ab90ffac47f",
+    "url": "/static/js/2.b3a0c0bd.chunk.js"
+  },
+  {
+    "revision": "66b91629dffba9d08872",
+    "url": "/static/js/main.2f67462c.chunk.js"
+  },
+  {
+    "revision": "934ed7fcc11c9ae52722",
+    "url": "/static/js/runtime-main.125738aa.js"
+  },
+  {
+    "revision": "c449bb8f19d7e20d6956edce2bf06089",
+    "url": "/static/media/logo.c449bb8f.jpg"
+  }
+]);
